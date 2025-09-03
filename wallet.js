22ebfe885395067eb79eefdbe61e112a223a4a37
biigbang0001 <134153650+biigbang0001@users.noreply.github.com>
2025-09-04 01:21:12 +0200

    Update wallet.js

--- wallet.js
+++ wallet.js
@@ -708,12 +708,18 @@ function importHDWallet(seedOrXprv, passphrase = '') {
 
 
     const bech32Node = hdWallet.derivePath("m/84'/0'/0'/0/0");
+
+
+    const legacyNode = hdWallet.derivePath("m/44'/0'/0'/0/0");
+
+
+    const p2shNode   = hdWallet.derivePath("m/49'/0'/0'/0/0");
     const pubkey = Buffer.from(bech32Node.publicKey);
     const keyPair = ECPair.fromPrivateKey(bech32Node.privateKey, { network: BC2_NETWORK });
 
-    const p2pkh = bitcoin.payments.p2pkh({ pubkey: pubkey, network: BC2_NETWORK });
+    const p2pkh = bitcoin.payments.p2pkh({ pubkey: Buffer.from(legacyNode.publicKey), network: BC2_NETWORK  });
     const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: pubkey, network: BC2_NETWORK });
-    const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: BC2_NETWORK });
+    const p2sh = bitcoin.payments.p2sh({ redeem: bitcoin.payments.p2wpkh({ pubkey: Buffer.from(p2shNode.publicKey), network: BC2_NETWORK }), network: BC2_NETWORK });
 
 
     const taprootNode = hdWallet.derivePath("m/86'/0'/0'/0/0");
@@ -746,9 +752,9 @@ function importWIF(wif) {
   try {
     const kp = ECPair.fromWIF(wif, BC2_NETWORK);
     const pubkeyBuffer = Buffer.from(kp.publicKey);
-    const p2pkh = bitcoin.payments.p2pkh({ pubkey: pubkeyBuffer, network: BC2_NETWORK });
+    const p2pkh = bitcoin.payments.p2pkh({ pubkey: Buffer.from(legacyNode.publicKey), network: BC2_NETWORK  });
     const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: pubkeyBuffer, network: BC2_NETWORK });
-    const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: BC2_NETWORK });
+    const p2sh = bitcoin.payments.p2sh({ redeem: bitcoin.payments.p2wpkh({ pubkey: Buffer.from(p2shNode.publicKey), network: BC2_NETWORK }), network: BC2_NETWORK });
     return {
       legacy: p2pkh.address,
       p2sh: p2sh.address,
@@ -772,7 +778,10 @@ function importHex(hex) {
     const pubkeyBuffer = Buffer.from(kp.publicKey);
     const p2pkh = bitcoin.payments.p2pkh({ pubkey: pubkeyBuffer, network: BC2_NETWORK });
     const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: pubkeyBuffer, network: BC2_NETWORK });
-    const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: BC2_NETWORK });
+    const p2sh = bitcoin.payments.p2sh({
+  redeem: bitcoin.payments.p2wpkh({ pubkey: Buffer.from(p2shNode.publicKey), network: BC2_NETWORK }),
+  network: BC2_NETWORK 
+});
     return {
       legacy: p2pkh.address,
       p2sh: p2sh.address,
@@ -985,7 +994,10 @@ function tweakSigner(signer, opts = {}) {
 function getP2SHAddress(pubkeyBuffer) {
   try {
     const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: pubkeyBuffer, network: BC2_NETWORK });
-    const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: BC2_NETWORK });
+    const p2sh = bitcoin.payments.p2sh({
+  redeem: bitcoin.payments.p2wpkh({ pubkey: Buffer.from(p2shNode.publicKey), network: BC2_NETWORK }),
+  network: BC2_NETWORK 
+});
     return { address: p2sh.address, redeemScript: p2wpkh.output };
   } catch (e) {
     console.error('Error converting to P2SH:', e);
@@ -1700,12 +1712,21 @@ window.addEventListener('load', async () => {
 
 
         const bech32Node = hdWallet.derivePath("m/84'/0'/0'/0/0");
+
+
+        const legacyNode = hdWallet.derivePath("m/44'/0'/0'/0/0");
+
+
+        const p2shNode   = hdWallet.derivePath("m/49'/0'/0'/0/0");
         const pubkey = Buffer.from(bech32Node.publicKey);
         const keyPair = ECPair.fromPrivateKey(bech32Node.privateKey, { network: BC2_NETWORK });
 
         const p2pkh = bitcoin.payments.p2pkh({ pubkey: pubkey, network: BC2_NETWORK });
         const p2wpkh = bitcoin.payments.p2wpkh({ pubkey: pubkey, network: BC2_NETWORK });
-        const p2sh = bitcoin.payments.p2sh({ redeem: p2wpkh, network: BC2_NETWORK });
+        const p2sh = bitcoin.payments.p2sh({
+  redeem: bitcoin.payments.p2wpkh({ pubkey: Buffer.from(p2shNode.publicKey), network: BC2_NETWORK }),
+  network: BC2_NETWORK 
+});
 
 
         const taprootNode = hdWallet.derivePath("m/86'/0'/0'/0/0");
